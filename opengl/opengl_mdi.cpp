5935c3125b86b3843b7a26088879ce3e3fb3629f
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-01-21 12:15:02 +0000

    Functional Qt + OpenGL example

--- opengl/opengl_mdi.cpp
+++ opengl/opengl_mdi.cpp
@@ -1,15 +1,44 @@
+#include <iostream>
+
 #include <QApplication>
 #include <QMenuBar>
 #include <QMainWindow>
+#include <QMdiArea>
+#include <QGLWidget>
+#include <QMdiSubWindow>
+
+class PlainGLWidget : public QGLWidget
+{
+protected:
+    virtual void paintGL();
+};
+
+void PlainGLWidget::paintGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
 
 int main(int argc, char *argv[]) {
-  QApplication app(argc, argv);
-  QMainWindow *window = new QMainWindow;
 
-  QMenuBar *menubar = window->menuBar();
-  menubar->addMenu("&File");
+    QApplication app(argc, argv);
 
-  window->show();
-  window->raise();
-  return app.exec();
+    QMainWindow *window = new QMainWindow;
+    
+    QMdiArea *mdi = new QMdiArea;
+    window->setCentralWidget(mdi);
+
+    PlainGLWidget *viewer = new PlainGLWidget;
+    viewer->resize(600,600);
+    
+    QMdiSubWindow *sub = new QMdiSubWindow;
+    sub->setWidget(viewer);
+    mdi->addSubWindow(sub);
+    sub->resize(600,600);
+    
+    window->show();
+    window->raise();
+
+    return app.exec();
+
 }
